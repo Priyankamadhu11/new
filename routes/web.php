fe699457005a56f1929876acf4c182de53dfdcee
root <root@server.aonepayment.com>
2024-02-15 07:19:31 -0500

    gitingre

--- routes/web.php
+++ routes/web.php
@@ -342,6 +342,4 @@ Route::get('payment-fail', 'Customer\PaymentController@fail')->name('payment-fai
 Route::get('/test', function (){
     return view('welcome');
 });
-Route::get('/web', function (){
-    dd("dev");
-});
+

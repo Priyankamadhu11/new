bf77b710ec7d80267b53746ca6306962059b2b41
priyanka <priyanka.zigainfotech@gmail.com>
2024-02-16 18:03:32 +0530

    ddd

--- app/Http/Controllers/Admin/SellerController.php
+++ app/Http/Controllers/Admin/SellerController.php
@@ -470,8 +470,8 @@ class SellerController extends Controller
             $seller_id=$id;
             if($seller_id)
             {
-                Coupon::where(['coupon_bearer'=>'seller','seller_id'=>$seller->id])->delete();
-                ImageManager::delete('/seller/' . $seller['image']);
+                // Coupon::where(['coupon_bearer'=>'seller','seller_id'=>$seller->id])->delete();
+                // ImageManager::delete('/seller/' . $seller['image']);
                 $seller->delete_status = 1;
                 $seller->delete();
                 Toastr::success(translate('Seller deleted successfully.'));
